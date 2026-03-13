4e1fb5fe6d38f81729a3a4d122282183648122e5
sergey151004 <sergey15perm@gmail.com>
2026-03-14 03:48:24 +0500

    delete news_service

--- news_service/news/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import NewsListView
-
-urlpatterns = [
-    path('news/', NewsListView.as_view(), name='news-list'),
-]
--- news_service/news/views.py
+++
@@ -1,9 +0,0 @@
-# news/views.py
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-
-class NewsListView(APIView):
-    """Временная заглушка — чтобы makemigrations прошёл"""
-    def get(self, request):
-        return Response({"message": "News service is alive"})
